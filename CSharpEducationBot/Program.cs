cf6fa75855fb390b842cfc92d987407810e88cef
hade5 <xhade5x@yandex.ru>
2018-05-04 23:24:56 +0300

    Добавлен логгер с настройками и простыми примерами в коде

--- CSharpEducationBot/Program.cs
+++ CSharpEducationBot/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace CSharpEducationBot
 {
     class Program
     {
+        static Logger log = LogManager.GetCurrentClassLogger(); // логгер
+
+
         static void Main(string[] args)
         {
+            log.Info("Запуск консоли");
+
+            log.Info("Остановка консоли");
         }
     }
 }
