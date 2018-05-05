be34ff7d9e1d9e96657abddf031b1e741f72db12
RenZ <ren.zigan@gmail.com>
2018-05-05 19:53:06 +0300

    Что сделано: 1. Добавлено два статических класса GetToken и ConnectionTest 2. Задача класса GetToken считать с файла токен бота и инициализировать экземпляр клиента(TelegramBotClient bot). Метод класса возвращает true (если успешно) или false (если возникли исключения). 3. Задача класса ConnectionTest проверить соединение с сервером. Проверка осуществляется с помощью метода await bot.GetMeAsync() включенного в библиотеку Telegram.Bot. Если соединие с сервером успешно, данный метов возвращает объект типа User (описание типов можно посмотреть в документации Телеграм API), если же сервер недоступен, метод вернут null, что вызовет дальнейшее исключение. Данный метод так же является проверочной и для валидностми самого токена, если токен недействительный, то сервер об этом оповестит. 4. Выше описанные классы применяются на старте приложения, если методы обоих классов вернули true - значит клиент настроен корректно. Если хотя из одного метода вернулось значение false, дальнейшее выполнение программы невозможно.

--- CSharpEducationBot/Program.cs
+++ CSharpEducationBot/Program.cs
@@ -4,20 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NLog;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
 
 namespace CSharpEducationBot
 {
-    class Program
+    internal class Program
     {
-        static Logger log = LogManager.GetCurrentClassLogger(); // логгер
+        static internal Logger log = LogManager.GetCurrentClassLogger(); // логгер
 
+        private static TelegramBotClient bot;
 
         static void Main(string[] args)
         {
             log.Info("Запуск консоли");
 
-            log.Info("Остановка консоли");
+            if (initBot().Result)
+            {
+                log.Info("Проверка завершена.");
+                //bot.StartReceiving(); запуск опроса чата
+                //to do
+            }
+            else
+            {
+                log.Info("Проверка завершилась с ошибками. Попробуйте перезапустить клиент.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
             Console.ReadLine();
+            log.Info("Остановка консоли");
+        }
+
+        //метод проверяет наличие токена, его валидность и доступность сервера, если в процессе проверки просиходит ошибка, управление 
+        //данному методу не возвращается - приложение выводит на экран логи ошибок и закрывается, именно поэтому метод всегда возвращает true
+        private async static Task<bool> initBot()
+        {
+            log.Info("Запуск проверки валидности токена и доступности сервера.");
+
+            if (GetToken.loadToken(ref bot) && await ConnectionTest.Test(bot))
+                return true;
+            return false;
         }
     }
 }
