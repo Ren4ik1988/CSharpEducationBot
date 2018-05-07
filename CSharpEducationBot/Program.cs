9d4c00215399543ea4adb0ba4edd2cdb82773fd5
Hade5 <xHade5X@yandex.ru>
2018-05-07 14:29:00 +0300

    Классы инициализации пермещены в отдельную папку. Добавлена система обработки комманд

--- CSharpEducationBot/Program.cs
+++ CSharpEducationBot/Program.cs
@@ -8,6 +8,7 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Args;
+using CSharpEducationBot.Commands; // подключаем простанство имен, где лежат команды 
 
 namespace CSharpEducationBot
 {
@@ -16,6 +17,10 @@ namespace CSharpEducationBot
         static internal Logger log = LogManager.GetCurrentClassLogger(); // логгер
 
         private static TelegramBotClient bot;
+
+        // список команд
+        private static List<Command> commandsList;
+
 
         static void Main(string[] args)
         {
@@ -40,11 +45,15 @@ namespace CSharpEducationBot
             log.Info("Остановка консоли");
         }
 
-        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        private static void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             Message msg = e.Message; // получаем сообщение
-            
-            await bot.SendTextMessageAsync(msg.Chat.Id, msg.Text);
+
+            // поиск класса у которого поле Name совпадает с входящей командой
+            Command RunCommand = commandsList.Find(s => s.Name == msg.Text);
+            if (RunCommand != null)
+                RunCommand.Execute(msg, bot);
+
         }
 
         //метод проверяет наличие токена, его валидность и доступность сервера, если в процессе проверки просиходит ошибка, управление 
@@ -54,7 +63,12 @@ namespace CSharpEducationBot
             log.Info("Запуск проверки валидности токена и доступности сервера.");
 
             if (GetToken.loadToken(ref bot) && await ConnectionTest.Test(bot))
+            {
+                // загружаем список команд
+                CommandsListInit.loadCommands(ref commandsList);
+
                 return true;
+            }
             return false;
         }
     }
