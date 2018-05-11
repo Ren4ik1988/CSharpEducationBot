2d2f236c758f5b7f7d316862d7a9186d632e35ce
= <=>
2018-05-11 13:27:33 +0300

    ﻿using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks; using NLog; using Telegram.Bot; using Telegram.Bot.Types; using Telegram.Bot.Exceptions; using Telegram.Bot.Args; using CSharpEducationBot.Commands; // подключаем простанство имен, где лежат команды
    namespace CSharpEducationBot
    {
        internal class Program
        {
            static internal Logger log = LogManager.GetCurrentClassLogger(); // логгер
    
            private static TelegramBotClient bot;
    
            // список команд
            private static List<Command> commandsList;
    
            static void Main(string[] args)
            {
                log.Info("Запуск консоли");
    
                if (initBot().Result)
                {
                    log.Info("Проверка завершена.");
    
                    bot.OnMessage += Bot_OnMessage; // присваиваем событие
    
                    bot.StartReceiving(); //запуск опроса чата
                }
                else
                {
                    log.Info("Проверка завершилась с ошибками. Попробуйте перезапустить клиент.");
                    Console.ReadLine();
                    Environment.Exit(0);
                }
    
                Console.ReadLine();
                log.Info("Остановка консоли");
            }
    
            private static void Bot_OnMessage(object sender, MessageEventArgs e)
            {
                Message msg = e.Message; // получаем сообщение
    
                // поиск класса у которого поле Name совпадает с входящей командой
                Command RunCommand = commandsList.Find(s => s.Name.ToLower().Contains(msg.Text.ToLower()));
                if (RunCommand != null)
                    RunCommand.Execute(msg, bot);
    
            }
    
            //метод проверяет наличие токена, его валидность и доступность сервера, если в процессе проверки просиходит ошибка, управление
            //данному методу не возвращается - приложение выводит на экран логи ошибок и закрывается, именно поэтому метод всегда возвращает true
            private async static Task<bool> initBot()
            {
                log.Info("Запуск проверки валидности токена и доступности сервера.");
    
                if (GetToken.loadToken(ref bot) && await ConnectionTest.Test(bot))
                {
                    // загружаем список команд
                    CommandsListInit.loadCommands(ref commandsList);
    
                    return true;
                }
                return false;
            }
        }
    }

---
+++ CSharpEducationBot/Commands/MenuCommand.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CSharpEducationBot.Commands
+{
+    class MenuCommand :Command
+    {
+        public override string Name => "/start";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            var mainMenu = new InlineKeyboardMarkup(new[]
+            {
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("Видео уроки", "video"),
+                    InlineKeyboardButton.WithCallbackData("Книги", "books"),
+                    InlineKeyboardButton.WithCallbackData("Поиск по всем ресурсам", "search")
+                }
+            });
+
+           
+
+            var mainBooksMenu = new InlineKeyboardMarkup(new[]
+            {
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("Видео уроки", "video"),
+                    InlineKeyboardButton.WithCallbackData("Книги", "books")
+                },
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("В главное меню", "MainMenu")
+                }
+            });
+
+            string MainText = "Привет! Данный бот поможет в поиске ресусов по С#. У нас есть видео и книги. Если ты пока не знаешь что выбрать, то воспользуйся общим поиском по разделам.";
+            await client.SendTextMessageAsync(chatId, MainText, replyMarkup: mainMenu);
+
+
+            client.OnCallbackQuery += async (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
+            {
+                var callBack = ev.CallbackQuery;
+                var msg = callBack.Message;
+
+                switch (callBack.Data)
+                {
+                    case "video":
+                        {
+                            var mainVideoMenu = new InlineKeyboardMarkup(new[]
+                            {
+                                new []
+                                {
+                                    InlineKeyboardButton.WithCallbackData("Видео уроки", "video"),
+                                    InlineKeyboardButton.WithCallbackData("Книги", "books")
+                                },
+                                new[]
+                                {
+                                    InlineKeyboardButton.WithCallbackData("В главное меню", "MainMenu")
+                                }
+                            });
+                            // список видео и ссылки на него получаем из БД
+                            await client.EditMessageTextAsync(chatId, messageId, "список видео", replyMarkup: mainVideoMenu);
+                            await client.AnswerCallbackQueryAsync(callBack.Id);
+                            break;
+                        }
+                    case "books":
+                        {
+                            await client.EditMessageTextAsync(chatId, messageId, "список книг", replyMarkup: mainBooksMenu);
+                            await client.AnswerCallbackQueryAsync(callBack.Id);
+                            break;
+                        }
+                    case "search":
+                        {
+                            await client.EditMessageTextAsync(chatId, messageId, "список книг", replyMarkup: mainMenu);
+                            await client.AnswerCallbackQueryAsync(callBack.Id);
+                            break;
+                        }
+                    case "MainMenu":
+                        {
+                            await client.SendTextMessageAsync(chatId, MainText, replyMarkup: mainMenu);
+                            await client.AnswerCallbackQueryAsync(callBack.Id);
+                            break;
+                        }
+                }
+            };
+        }
+    }
+}
