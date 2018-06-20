f4bf776c1df926bbb010fdfb02349d3669cb0c04
= <=>
2018-06-20 14:45:37 +0300

    Добавлена возможность использовать проски. Добавлен файл настройки.

--- CSharpEducationBot/Init/GetToken.cs
+++ CSharpEducationBot/Init/GetToken.cs
@@ -5,19 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using MihaZupan;
+using Newtonsoft.Json;
+using Telegram.Bot.Types;
 
 namespace CSharpEducationBot
 {
     static class GetToken //считывает токен из файла и проверяет его на валидность, 
                    //если удачно - создается экземляр бота, если неудачно - программа закрывается
     {
+        static Properties.Settings param = new Properties.Settings();
+
         private const string filePath = "key.txt";
 
         internal static bool loadToken(ref TelegramBotClient bot)
         {
             try
             {
-                bot = new TelegramBotClient(readToken()); //инициализируем клиент телеграм бота
+                param.Reload(); // читаем параметры из файла приложения, если этого не делать, то будут настройки по умолчанию (т.е. указанные при создании файла)
+
+                // Определяем способ подключения к серверам
+                if (param.usesocks5 && 
+                   !String.IsNullOrEmpty(param.socksip)  && param.socksport != 0)
+                {
+                    var proxy = new HttpToSocks5Proxy(param.socksip, param.socksport);
+                    proxy.ResolveHostnamesLocally = true;
+                    bot = new TelegramBotClient(readToken(), proxy);
+                }
+                else
+                    bot = new TelegramBotClient(readToken()); //инициализируем клиент телеграм бота
             }
             catch ( ArgumentException ex) // если файл найден, но токен имел неверный формат
             {
